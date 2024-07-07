f4bf1f980593b021b88259208ffc7e40d69eced5
Sumnicky99 <sumnicky99@gmail.com>
2024-07-07 09:49:34 +0300

    code along

---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function App() {
+  return <h1>Hello, React!</h1>;
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
